7177f2608c64c14ac062e66f4e1fb5e34a5d4461
subhransupk <your.email@example.com>
2025-03-11 11:56:08 +0530

    Updated next.config.mjs and package.json, added vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000', 'localhost:3001', 'localhost:3002']
+      allowedOrigins: ['localhost:3000', 'localhost:3001', 'localhost:3002', 'vercel.app']
     }
   },
   transpilePackages: ['@tremor/react', 'recharts'],
   images: {
-    domains: ['dummyimage.com', 'placehold.co'],
+    domains: ['dummyimage.com', 'placehold.co', 'vercel.app'],
+    unoptimized: process.env.NODE_ENV === 'production',
   }
 };
 
